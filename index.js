d798abe1f89663651954fb1ac6003aa194ffa53e
Praveen Kumar <pk46066@gmail.com>
2025-10-29 17:06:56 +0530

    Basic Terminal Chat App with OpenAI

--- index.js
+++ index.js
@@ -2,14 +2,12 @@ import openAi from 'openai';
 import dotenv from 'dotenv';
 dotenv.config();
 
-import { encoding_for_model } from 'tiktoken';
+// import { encoding_for_model } from 'tiktoken';
 
-const openai = new openAi({
-  apiKey: process.env.openAIKey,
-});
+const client = new openAi({apiKey : process.env.openAIKey});
 
 /*
-const response= await openai.responses.create({
+const response= await client.responses.create({
   instructions: "give result in maximum 10 words",
   input: "BMW",
   model: "gpt-4o-mini"
@@ -19,10 +17,11 @@ const response= await openai.responses.create({
   // ],
 }); */
 
+/* calculateTokens
 const prompt ='node js backend developer ?';
 const model = 'gpt-4o-mini';
 
-const response= await openai.responses.create({
+const response= await client.responses.create({
   input: [
     // { role: "system", content: "answer in english language" },
     // { role: "developer", content: "give a basic example in nodejs" },
@@ -41,3 +40,32 @@ function calculateTokens() {
 }
 
 calculateTokens() 
+*/
+// Basic Terminal Chat App with OpenAI START
+async function aiAnswer(aiQue) {
+    const response = await client.responses.create({
+      model: "gpt-4o-mini",
+      input: aiQue
+    });
+    
+  console.log(response.output_text);
+}
+
+process.stdout.write("Starting chat...\n");
+process.stdin.on('data', async (data) => {
+const aiQue = data.toString().trim();
+if (aiQue.toLowerCase() === 'exit') {
+    process.stdout.write("Exiting chat...\n");
+    process.exit();
+}
+
+aiAnswer(aiQue)
+})
+
+
+
+// myChat().then(response => {
+//   console.log(response.output_text);
+// }).catch(error => {
+//   console.error("Error:", error);
+// });
